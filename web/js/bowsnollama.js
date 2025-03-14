2ad83985de68c258a8fda2c6a38a73397ccd54c2
jeffyactive <jeff@reelyactive.com>
2025-03-14 06:16:42 -0400

    Added connect icon for WebSocket status

--- web/js/bowsnollama.js
+++ web/js/bowsnollama.js
@@ -7,9 +7,11 @@ let websocket = new WebSocket(window.location.href);
 
 
 websocket.addEventListener('open', () => {
+  connectIcon.setAttribute('class', 'bi-cloud-fill text-success');
   console.log('Connected to WebSocket on', websocket.url);
 });
 websocket.addEventListener('message', (event) => {
+  connectIcon.setAttribute('class', 'bi-cloud-arrow-up text-success');
   try {
     let message = JSON.parse(event.data);
     responseText.textContent += message.data;
@@ -17,9 +19,11 @@ websocket.addEventListener('message', (event) => {
   catch(err) {}
 });
 websocket.addEventListener('error', (event) => {
+  connectIcon.setAttribute('class', 'bi-cloud-fill text-warning');
   console.log('WebSocket error');
 });
 websocket.addEventListener('close', (event) => {
+  connectIcon.setAttribute('class', 'bi-cloud-fill text-danger');
   console.log('Disconnected from WebSocket on', websocket.url);
 });
 
